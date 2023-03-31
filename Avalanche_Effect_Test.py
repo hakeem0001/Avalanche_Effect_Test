5bb717b05c7aecea4a3294512f13ae79b16605b5
Hakeem Imad <106028453+hakeem0001@users.noreply.github.com>
2023-03-31 06:06:05 +0300

    A simple program to run Avalanche Effect Test
    Text and String

---
+++ Avalanche_Effect_Test.py
@@ -0,0 +1,11 @@
+from pyavalanche import PyAvalanche
+# Avalanche Effect Test by hakeem
+
+c1 ="Yut6U504lMufZVidv6K2EesrOqVZRb7eB49jw9CRLfaQuzNKniU4CmA0/ofjdyIY"
+
+c2 ="ZvFc65C4bhGx0UtByP4Tqw4wOdR8HiHBbladq4sHS3vrtm+2sIp1mQbmQib5h^x3"
+
+
+ava = PyAvalanche()
+
+print(ava.avalanche_effect(c1, c2))
